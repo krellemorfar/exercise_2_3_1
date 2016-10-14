924f62c30e0219487d65a31fca485fb6d39a96e8
Kristoffer Winther Jessen <krelle@cnfa.dk>
2016-10-14 17:47:07 +0200

    Created Person and Movie classes

---
+++ src/Exercise_2_3_1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise_2_3_1
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
